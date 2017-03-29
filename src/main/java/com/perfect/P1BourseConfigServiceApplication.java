8aad55ae48ba40c0bca36bd744a0fca34118764f
riyad <riyad>
2017-03-29 11:22:42 +0300

    first commit

---
+++ src/main/java/com/perfect/P1BourseConfigServiceApplication.java
@@ -0,0 +1,12 @@
+package com.perfect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1BourseConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1BourseConfigServiceApplication.class, args);
+	}
+}
